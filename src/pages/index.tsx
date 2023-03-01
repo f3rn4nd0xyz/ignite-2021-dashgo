9908d3876ce11089144b9ff82a5a6a9ff8c6ce56
f3rn4nd0xyz <contasuch123@gmail.com>
2023-03-01 02:27:26 -0300

    login, dashboard and users control pages created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import { Input } from "@/components/Form/Input";
+import { Flex, Button, Stack } from "@chakra-ui/react";
+
+export default function SignIn() {
   return (
-    <>
-      <h1>Dashgo</h1>
-    </>
-  )
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Password" />
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
